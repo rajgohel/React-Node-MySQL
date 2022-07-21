a153a74fc74e16e831cc8ac9a5d9f70db808ef8e
Raj Gohel <rajgohel0001@gmail.com>
2022-07-21 11:26:41 +0530

    Employee CRUD API completed

--- server/app/controllers/employee.controller.js
+++ server/app/controllers/employee.controller.js
@@ -2,7 +2,7 @@ const db = require("../models");
 const Employee = db.employees;
 const Op = db.Sequelize.Op;
 
-// Retrieve all Tutorials from the database.
+// Retrieve all Employees from the database.
 exports.findAll = (req, res) => {
   const title = req.query.title;
   var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
@@ -15,6 +15,89 @@ exports.findAll = (req, res) => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while retrieving employees."
+      });
+    });
+};
+
+// Create and Save a new Employee
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.firstName) {
+    res.status(400).send({
+      message: "First Name can not be empty!"
+    });
+    return;
+  }
+
+  // Create a Employee
+  const employee = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    address: req.body.address,
+    dob: req.body.dob,
+    mobile: req.body.mobile,
+    city: req.body.city
+  };
+
+  // Save Employee in the database
+  Employee.create(employee)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Employee."
+      });
+    });
+};
+
+// Update a Employee by the id in the request
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  Employee.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Employee was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update Employee with id=${id}. Maybe Employee was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Employee with id=" + id
+      });
+    });
+};
+
+// Delete a Employee with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Employee.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Employee was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Employee with id=${id}. Maybe Employee was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Employee with id=" + id
       });
     });
 };
--- server/app/routes/employee.routes.js
+++ server/app/routes/employee.routes.js
@@ -1,8 +1,19 @@
 module.exports = app => {
-    const employees = require("../controllers/employee.controller.js");
-  
-    var router = require("express").Router();
-    // Retrieve all Employees
-    router.get("/", employees.findAll);
-  };
-  
+  const employees = require("../controllers/employee.controller.js");
+
+  var router = require("express").Router();
+
+  // Retrieve all Employees
+  router.get("/", employees.findAll);
+
+  // Create a new Employee
+  router.post("/", employees.create);
+
+  // Update a Employee with id
+  router.put("/:id", employees.update);
+
+  // Delete a Employee with id
+  router.delete("/:id", employees.delete);
+
+  app.use('/api/employees', router);
+};
